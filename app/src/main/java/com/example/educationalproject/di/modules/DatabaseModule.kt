4ed0898690d2b9f1469981eb3994ed7c26462687
Bislan <mamaliev.bislan@mail.ru>
2022-09-26 10:59:26 +0300

    Module 37 done! Dagger has been implemented.

---
+++ app/src/main/java/com/example/educationalproject/di/modules/DatabaseModule.kt
@@ -0,0 +1,13 @@
+package com.example.educationalproject.di.modules
+
+import com.example.educationalproject.data.MainRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRepository() = MainRepository()
+}
